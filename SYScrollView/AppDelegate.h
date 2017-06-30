8342597ec748c1251b959b165b5d784eb984d3eb
刘世玉 <1010530278@qq.com>
2017-07-01 00:04:39 +0800

    no message

---
+++ SYScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYScrollView
+//
+//  Created by 流诗语 on 2017/6/30.
+//  Copyright © 2017年 刘世玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
